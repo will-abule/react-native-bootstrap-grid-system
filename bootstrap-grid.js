27982b48b9ffa4c989fd131d984f6d7e2cb41045
Will Abule <willxd42@gmail.com>
2019-10-21 16:00:43 +0100

    made fully compartable with react native

--- bootstrap-grid.js
+++ bootstrap-grid.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingRight: 15,
     paddingLeft: 15,
-    marginright: "auto",
+    marginRight: "auto",
     marginLeft: "auto"
   },
 
